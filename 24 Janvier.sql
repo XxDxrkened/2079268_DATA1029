528ebf98801f0638bfbdf0927de9a3d0e47cebc6
XxDxrkened <157416101+XxDxrkened@users.noreply.github.com>
2024-01-24 11:30:10 -0400

    Delete 24 Janvier.sql

--- 24 Janvier.sql
+++
@@ -1,9 +0,0 @@
-SELECT year(pubdate) FROM titles GROUP BY year(pubdate);
-
-SELECT type, count(*) FROM titles GROUP BY type;
-
-SELECT type, count(DISTINCT pub_id) FROM titles GROUP BY type;
-
-SELECT type, COUNT(DISTINCT pub_id) AS "Nombre d’éditeurs" FROM titles GROUP BY type;
-
-SELECT country, COUNT(*) FROM publishers WHERE country like "%r%" GROUP BY country HAVING COUNT(*);
