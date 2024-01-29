235814d0a9ea23cf7ee7c7c9933ffdbee582c3c3
XxDxrkrned <nikoslegeremelanson@gmail.com>
2024-01-28 21:30:06 -0400

    Exercice 6 Complété

--- devoir_semaine_2/devoir_semaine_2.sql
+++ devoir_semaine_2/devoir_semaine_2.sql
@@ -13,3 +13,6 @@ SELECT title, price FROM titles WHERE NOT (title LIKE 'SU%' OR title LIKE 'BU%')
 
 -- Exercice 5: À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la seconde lettre est « o ». Afficher le titre et le prix. (15 pts)
 SELECT title, price FROM titles WHERE (title LIKE '_o%') AND (title NOT LIKE 'S%' OR title NOT LIKE 'B%')  GROUP BY title_id;
+
+-- Ecercice 6: À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la 3ème lettre est « f ». Afficher le titre et le prix.
+SELECT title, price FROM titles WHERE (title LIKE '__f%') AND (title NOT LIKE 'S%' OR title NOT LIKE 'B%')  GROUP BY title_id;
