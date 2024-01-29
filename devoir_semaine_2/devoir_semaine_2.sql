5a93adb02bf0468ce9847e48ea97ef48bccbe789
XxDxrkrned <nikoslegeremelanson@gmail.com>
2024-01-28 21:40:32 -0400

    Exercice 7 Complété

--- devoir_semaine_2/devoir_semaine_2.sql
+++ devoir_semaine_2/devoir_semaine_2.sql
@@ -14,5 +14,8 @@ SELECT title, price FROM titles WHERE NOT (title LIKE 'SU%' OR title LIKE 'BU%')
 -- Exercice 5: À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la seconde lettre est « o ». Afficher le titre et le prix. (15 pts)
 SELECT title, price FROM titles WHERE (title LIKE '_o%') AND (title NOT LIKE 'S%' OR title NOT LIKE 'B%')  GROUP BY title_id;
 
--- Ecercice 6: À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la 3ème lettre est « f ». Afficher le titre et le prix.
+-- Ecercice 6: À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la 3ème lettre est « f ». Afficher le titre et le prix. (15 pts)
 SELECT title, price FROM titles WHERE (title LIKE '__f%') AND (title NOT LIKE 'S%' OR title NOT LIKE 'B%')  GROUP BY title_id;
+
+-- Exercice 7: À partir de la table « titles », obtenir la liste des titres débutant par l’une des 10 premières lettres de l’alphabet. Afficher le titre et le prix. (15 pts)
+SELECT title, price FROM titles WHERE title RLIKE '^[A-J]' GROUP BY title_id;
