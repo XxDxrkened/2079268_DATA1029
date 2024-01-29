47e23063477aa69378e7d354cf673a995ae21276
XxDxrkrned <nikoslegeremelanson@gmail.com>
2024-01-28 20:41:58 -0400

    Exercice 3 Complété

--- devoir_semaine_2/devoir_semaine_2.sql
+++ devoir_semaine_2/devoir_semaine_2.sql
@@ -4,3 +4,6 @@ SELECT title, price FROM titles WHERE title LIKE '%computer%' GROUP BY title_id;
 
 -- Exercice 2: À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » (au singulier). Afficher le titre et le prix de chaque livre. (15 pts)
 SELECT title, price FROM titles WHERE title LIKE '%computer%' AND title NOT LIKE '%computers%' GROUP BY title_id;
+
+-- Exercice 3: À partir de la table « titles », obtenir la liste des titres dont l’identifiant débute par SU ou BU. Afficher le titre et le prix de chaque livre. (15 pts)
+SELECT title, price FROM titles WHERE title LIKE 'SU%' OR title LIKE 'BU%' GROUP BY title_id;
