1edf3b6d51d378b40e3b70ca89b331984a0598a1
XxDxrkrned <nikoslegeremelanson@gmail.com>
2024-01-28 20:01:39 -0400

    Updated

---
+++ devoir_semaine_2/exercice_2.sql
@@ -0,0 +1,3 @@
+USE library;
+-- Exercice 2: À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » (au singulier). Afficher le titre et le prix de chaque livre. (15 pts)
+SELECT title, price FROM titles WHERE title LIKE '%computer%' AND title NOT LiKE '%computers%';
