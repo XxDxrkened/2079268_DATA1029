4157661d4afc4bc6526ccd6239e6a8495818cd93
XxDxrkened <157416101+XxDxrkened@users.noreply.github.com>
2024-01-28 20:04:40 -0400

    Delete devoir_semaine_2/exercice_1.sql

--- devoir_semaine_2/exercice_1.sql
+++
@@ -1,3 +0,0 @@
-use library;
--- Exercice 1: À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » Afficher le titre et le prix de chaque livre. (10pts)
-SELECT title, price FROM titles WHERE title LIKE '%computer%' GROUP BY title_id;
