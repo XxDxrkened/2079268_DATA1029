8ed073bd738ad7ed77767f964196afa248bf053e
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-04 11:23:40 -0400

    Revert "Create devoir_semaine_3.sql"
    This reverts commit dba31fa051b3731fe0afa4a6138a8d8740a9a2ce.

--- devoir_semaine_3/devoir_semaine_3.sql
+++
@@ -1,6 +0,0 @@
--- Question 1 : La liste des paires (auteur, éditeur) demeurant dans la même ville. (10pts)
-SELECT authors.au_fname AS prenom, authors.au_lname AS nom, publishers.pub_name AS publisher, authors.city
-FROM authors
-JOIN publishers ON authors.city = publishers.city
-AND authors.au_id <> publishers.pub_id;
-
