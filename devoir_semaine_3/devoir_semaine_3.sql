f3a2dbcdd124492a2a9ffe849afc36662572f16d
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-04 12:11:15 -0400

    Question 3 Complété et Question 2 modifier

--- devoir_semaine_3/devoir_semaine_3.sql
+++ devoir_semaine_3/devoir_semaine_3.sql
@@ -7,5 +7,9 @@ AND authors.au_id <> publishers.pub_id;
 -- Question 2 : La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les auteurs qui ne répondent pas à ce critère. (10pts)
 SELECT authors.au_fname AS prenom, authors.au_lname AS nom, publishers.pub_name AS publisher, authors.city
 FROM authors
-LEFT JOIN publishers ON authors.city = publishers.city
-AND authors.au_id <> publishers.pub_id;
+LEFT JOIN publishers ON authors.city = publishers.city;
+
+-- Question 3 : La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les éditeurs qui ne répondent pas à ce critère. (10 pts)
+SELECT authors.au_fname AS prenom, authors.au_lname AS nom, publishers.pub_name AS publisher, authors.city
+FROM authors
+RIGHT JOIN publishers ON authors.city = publishers.city;
