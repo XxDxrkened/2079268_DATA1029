a9fb65573dfbbb2d14409046e2fe9a91c48c6dc8
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-04 12:03:32 -0400

    Question 2 Complété

--- devoir_semaine_3/devoir_semaine_3.sql
+++ devoir_semaine_3/devoir_semaine_3.sql
@@ -4,3 +4,8 @@ FROM authors
 JOIN publishers ON authors.city = publishers.city
 AND authors.au_id <> publishers.pub_id;
 
+-- Question 2 : La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les auteurs qui ne répondent pas à ce critère. (10pts)
+SELECT authors.au_fname AS prenom, authors.au_lname AS nom, publishers.pub_name AS publisher, authors.city
+FROM authors
+LEFT JOIN publishers ON authors.city = publishers.city
+AND authors.au_id <> publishers.pub_id;
