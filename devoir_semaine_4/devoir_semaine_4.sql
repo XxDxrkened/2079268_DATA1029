a7a2bd4f6491b6e6fa3da11df85680d5b22291bd
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-14 13:42:59 -0400

    Question 3 Complété

--- devoir_semaine_4/devoir_semaine_4.sql
+++ devoir_semaine_4/devoir_semaine_4.sql
@@ -21,3 +21,18 @@ LEFT JOIN titleauthor AS title_au ON t.title_id = title_au.title_id AND title_au
 WHERE p.pub_name = 'Eyrolles'
 AND title_au.title_id IS NULL
 );
+
+-- Question 3: Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan"
+SELECT a.au_fname, a.au_lname
+FROM authors AS a
+WHERE a.au_id IN (
+SELECT ta.au_id
+FROM titleauthor AS ta
+JOIN titles as t ON ta.title_id = t.title_id
+WHERE t.advance > ALL (
+SELECT t.advance
+FROM titles AS t
+JOIN publishers AS p ON t.pub_id = p.pub_id
+WHERE p.pub_name = 'Harmattan'
+)
+);
