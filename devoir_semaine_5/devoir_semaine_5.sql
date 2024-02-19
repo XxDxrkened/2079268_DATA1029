67b95deee6fc21a248fdcc3f622642555636adfb
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-19 16:44:07 -0400

    Créer la deuxième table pubishers

--- devoir_semaine_5/devoir_semaine_5.sql
+++ devoir_semaine_5/devoir_semaine_5.sql
@@ -6,15 +6,25 @@ USE library_devoir_semaine_5;
 
 DROP TABLE IF EXISTS authors;
 CREATE TABLE authors (
-	au_id tinyint NOT NULL AUTO_INCREMENT primary key,
-    au_lname varchar(50) NOT NULL,
-    au_fname varchar(50) NOT NULL,
-    phone varchar(15) NOT NULL UNIQUE CHECK (phone RLIKE '^+[0-9]{10,15}$'),
-    address varchar(50) NOT NULL,
-    city varchar(50) NOT NULL,
-    state varchar(50) NULL,
-    country varchar(50) NOT NULL,
-    zip char(6) NOT NULL CHECK (zip RLIKE '^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$'),
-    contract text NOT NULL,
-    email varchar(50) NOT NULL CHECK (email RLIKE '%@%') 
+au_id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+au_lname varchar(50) NOT NULL,
+au_fname varchar(50) NOT NULL,
+phone varchar(15) NOT NULL UNIQUE CHECK (phone RLIKE '^+[0-9]{10,15}$'),
+address varchar(50) NOT NULL,
+city varchar(50) NOT NULL,
+state varchar(50) NULL,
+country varchar(50) NOT NULL,
+zip char(6) NOT NULL CHECK (zip RLIKE '^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$'),
+contract text NOT NULL,
+email varchar(50) NOT NULL CHECK (email RLIKE '%@%') 
+);
+
+DROP TABLE IF EXISTS publishers;
+CREATE TABLE publishers (
+pub_id tinyint NOT NULL PRIMARY KEY,
+pub_name varchar(50) NOT NULL,
+city varchar(50) NOT NULL,
+state varchar(50) NULL,
+country varchar(50) NOT NULL,
+email varchar(50) NOT NULL CHECK (email RLIKE '%@%')
 );
